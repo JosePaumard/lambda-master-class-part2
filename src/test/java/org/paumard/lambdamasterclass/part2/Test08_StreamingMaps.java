851c0f1833cb8e9c9a2b5d37221f0a7b81285e8d
José <Jose.Paumard@gmail.com>
2018-11-12 23:49:52 +0100

    Cleaned up the examples for the demo

--- src/test/java/org/paumard/lambdamasterclass/part2/Test08_StreamingMaps.java
+++ src/test/java/org/paumard/lambdamasterclass/part2/Test08_StreamingMaps.java
@@ -21,11 +21,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class Test08_StreamingMaps {
 
-    private Set<String> dictionnaryWords;
-    private Set<String> shakespeareWords;
-    private Set<String> prideAndPrejudice;
-    private Set<String> prideAndPrejudiceLines;
-
     private List<String> sonnet = List.of(
             "From fairest creatures we desire increase,",
             "That thereby beauty's rose might never die,",
@@ -43,154 +38,13 @@ public class Test08_StreamingMaps {
             "To eat the world's due, by the grave and thee.",
             "to to thy");
 
-    @Before
-    public void before() {
-        Path dictionnary = Paths.get("data/ospd.txt.gz");
-        dictionnaryWords = readWordsFrom(dictionnary);
-
-        Path shakespeare = Paths.get("data/words.shakespeare.txt.gz");
-        shakespeareWords = readWordsFrom(shakespeare);
-
-        Path prideAndPrejudice = Paths.get("data/Pride-and-Prejudice.txt.gz");
-        prideAndPrejudiceLines = readWordsFrom(prideAndPrejudice);
-    }
-
-    private Set<String> readWordsFrom(Path dictionnary) {
-        try (GZIPInputStream gzipInputStream = new GZIPInputStream(Files.newInputStream(dictionnary));
-             BufferedReader reader = new BufferedReader(new InputStreamReader(gzipInputStream));
-             Stream<String> lines = reader.lines();) {
-
-            return lines.map(String::toLowerCase).collect(Collectors.toSet());
-
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return Set.of();
-    }
-
+    /**
+     * Find the most frequently occurring words in the Sonnet
+     */
     @Test
     public void streamingMaps_1() {
 
-        assertThat(dictionnaryWords.size()).isEqualTo(79339);
-        assertThat(shakespeareWords.size()).isEqualTo(23688);
-    }
-
-    /**
-     * Find the longest word from the Shakespeare words
-     */
-    @Test
-    public void streamingMaps_2() {
-
-        String longestWord = shakespeareWords.stream()
-                .max(Comparator.comparing(String::length)).get();
-        System.out.println("longestWord = " + longestWord);
-    }
-
-    /**
-     * Find the longest word from the Shakespeare words
-     * that is in the dictionnary
-     */
-    @Test
-    public void streamingMaps_3() {
-
-        String longestWord = shakespeareWords.stream()
-                .filter(word -> dictionnaryWords.contains(word))
-                .max(Comparator.comparing(String::length)).get();
-        System.out.println("longestWord = " + longestWord);
-    }
-
-
-    /**
-     * Find the most used Pride and Prejudice words of more than
-     * 4 letter
-     */
-    @Test
-    public void streamingMaps_4() {
-
-        IntPredicate isLetter = letter -> letter >= 'a' && letter <= 'z';
-        String nonLetters = prideAndPrejudiceLines.stream()
-                .flatMapToInt(line -> line.chars())
-                .filter(isLetter.negate())
-                .distinct()
-                .mapToObj(i -> String.valueOf((char) i))
-                .collect(Collectors.joining());
-        System.out.println("nonLetters = " + nonLetters);
-
-        Pattern pattern = Pattern.compile("[“ ,”';\\.\\?_\\-!:2436501879()\\*]+");
-
-        Set<String> words = prideAndPrejudiceLines.stream()
-                .flatMap(line -> pattern.splitAsStream(line))
-                .collect(Collectors.toSet());
-        System.out.println("words.size() = " + words.size());
-
-        Map<String, Long> wordsAppearance =
-                prideAndPrejudiceLines.stream()
-                        .flatMap(line -> pattern.splitAsStream(line))
-                        .collect(
-                                Collectors.groupingBy(
-                                        Function.identity(), Collectors.counting()
-                                )
-                        );
-
-        Map.Entry<String, Long> mostSeenWord = wordsAppearance.entrySet().stream()
-                .filter(entry -> entry.getKey().length() > 3)
-                .max(Map.Entry.comparingByValue())
-                .get();
-        System.out.println("mostSeenWord = " + mostSeenWord);
-
-//        Map<Long, List<Map.Entry<String, Long>>> collect =
-        Map<Long, List<String>> collect =
-                wordsAppearance.entrySet().stream()
-                        .filter(entry -> entry.getKey().length() > 3)
-                        .collect(Collectors.groupingBy(
-                                entry -> entry.getValue(),
-                                Collectors.mapping(entry -> entry.getKey(), Collectors.toList())));
-
-        Map.Entry<Long, List<String>> entry = collect.entrySet().stream()
-                .max(Map.Entry.comparingByKey())
-                .get();
-        System.out.println("entry = " + entry);
-
-    }
-
-    @Test
-    public void streamingMaps_5() {
-
         Pattern pattern = Pattern.compile(("[ ,':\\-]+"));
-
-        Map<String, Long> map = sonnet.stream()
-                .map(String::toLowerCase)
-                .flatMap(line -> pattern.splitAsStream(line))
-                .collect(
-                        Collectors.collectingAndThen(
-                                Collectors.groupingBy(
-                                        Function.identity(),
-                                        Collectors.counting()
-                                ),
-                                Map::copyOf
-                        )
-
-                );
-        System.out.println("map.size() = " + map.size());
-
-        Map.Entry<String, Long> mostSeenWord =
-                map.entrySet().stream()
-                        .max(Map.Entry.comparingByValue())
-                        .get();
-        System.out.println("mostSeenWord = " + mostSeenWord);
-
-//        Map<Long, List<Map.Entry<String, Long>>> collect =
-        Map<Long, List<String>> collect = map.entrySet().stream()
-                .collect(
-                        Collectors.groupingBy(
-                                entry -> entry.getValue(),
-                                Collectors.mapping(entry -> entry.getKey(), Collectors.toList())));
-
-        Map.Entry<Long, List<String>> mostSeenWords = collect.entrySet().stream()
-                .max(Map.Entry.comparingByKey())
-                .get();
-        System.out.println("mostSeenWords = " + mostSeenWords);
-
 
     }
 }
