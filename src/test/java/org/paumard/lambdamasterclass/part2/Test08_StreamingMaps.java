79272b1bbebf33297993d95eeba4fd13e37cf75d
Dinh Bao Dang <bdang@pro-vision.de>
2018-11-13 12:56:23 +0100

    add a dot to the Test08 regex so this is also filtered...
    "thee." will be in the list. Not a deal breaker for the counting exercise. See:
    
    assertThat(
            sonnet.stream().flatMap(pattern::splitAsStream)
    ).doesNotContain("thee.");

--- src/test/java/org/paumard/lambdamasterclass/part2/Test08_StreamingMaps.java
+++ src/test/java/org/paumard/lambdamasterclass/part2/Test08_StreamingMaps.java
@@ -44,7 +44,7 @@ public class Test08_StreamingMaps {
     @Test
     public void streamingMaps_1() {
 
-        Pattern pattern = Pattern.compile(("[ ,':\\-]+"));
+        Pattern pattern = Pattern.compile(("[ ,':\\-\\.]+"));
 
     }
 }
